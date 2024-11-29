cff3d0eb385307705af3f99bc639d0b222ab2ba0
이예성 <yeseong_lee@naver.com>
2024-11-30 03:15:15 +0900

    Initial Commit

---
+++ DropDrug/DropDrug.h
@@ -0,0 +1,18 @@
+//
+//  DropDrug.h
+//  DropDrug
+//
+//  Created by 이예성 on 11/30/24.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for DropDrug.
+FOUNDATION_EXPORT double DropDrugVersionNumber;
+
+//! Project version string for DropDrug.
+FOUNDATION_EXPORT const unsigned char DropDrugVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DropDrug/PublicHeader.h>
+
+
---
+++ DropDrugTests/DropDrugTests.swift
@@ -0,0 +1,17 @@
+//
+//  DropDrugTests.swift
+//  DropDrugTests
+//
+//  Created by 이예성 on 11/30/24.
+//
+
+import Testing
+@testable import DropDrug
+
+struct DropDrugTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
